1e7430d8754c70dc04b243a8628881cb596fcd88
DELL <DELL@DESKTOP-7SNL7GR>
2022-11-22 15:45:34 +0530

    abc

---
+++ src/test/java/PractisePackage/Sample1Test.java
@@ -0,0 +1,20 @@
+package PractisePackage;
+
+
+import org.testng.annotations.Test;
+
+public class Sample1Test {
+
+	
+		   
+		@Test
+		public void script1()
+		{
+			System.out.println("------------");
+			System.out.println("........................");
+			
+		
+		}
+}
+
+
