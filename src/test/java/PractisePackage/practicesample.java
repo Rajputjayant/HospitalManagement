ef87cf43f166214be2cbf4f792bc5817c3ca2071
AKII <AKII@AKII>
2022-11-23 17:33:28 +0530

    281

--- src/test/java/PractisePackage/practicesample.java
+++ src/test/java/PractisePackage/practicesample.java
@@ -5,6 +5,8 @@ public class practicesample {
 	public static void main(String[] args) {
 		System.out.println("adadsd");
 		System.out.println("apple");
+		System.out.println("apple25");
+		
 			
 
 	}
